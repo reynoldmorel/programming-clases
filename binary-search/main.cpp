64072467d06e6b9c55b262f6dbff33095d754723
Reynold Morel <rmorel@apap.com.do>
2020-07-26 12:07:31 -0400

    Adding Binary search exercise

--- binary-search/main.cpp
+++ binary-search/main.cpp
@@ -3,6 +3,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
+	// Create an algorythm that searches for a specific element in an int array
+	// Example: create an array with the following elements: 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20.
+	// Create a function number_exists(int n). This function will return 1 if the number exists and 0 if doesn't.
 	return 0;
 }
