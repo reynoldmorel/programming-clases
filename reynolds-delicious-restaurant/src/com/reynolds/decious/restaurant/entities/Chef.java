f1ad766e8c23086c7a444831208f9d349a9ae801
Reynold Morel <rmorel@apap.com.do>
2020-09-20 10:12:29 -0400

    Created project entities

---
+++ reynolds-delicious-restaurant/src/com/reynolds/decious/restaurant/entities/Chef.java
@@ -0,0 +1,11 @@
+package com.reynolds.decious.restaurant.entities;
+
+public class Chef extends Employee {
+    public Chef() {
+        super();
+    }
+
+    public Chef(Long id) {
+        super(id);
+    }
+}
