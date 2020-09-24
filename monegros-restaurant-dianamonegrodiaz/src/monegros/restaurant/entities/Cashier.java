39d6a246d553e83fdf279ef527b31794c83f6ee3
Diana Monegro <diana2monegro1@gmail.com>
2020-09-24 15:06:38 -0400

    Entities created for the kitchen project

---
+++ monegros-restaurant-dianamonegrodiaz/src/monegros/restaurant/entities/Cashier.java
@@ -0,0 +1,11 @@
+package monegros.restaurant.entities;
+
+public class Cashier extends Employee {
+    public Cashier() {
+        super();
+    }
+
+    public Cashier(Long id) {
+        super(id);
+    }
+}
