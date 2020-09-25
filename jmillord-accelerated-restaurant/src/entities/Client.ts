0da0df95849affc01d0f51277203ebaa1e97bb32
APAP-PDC\jmillord <juancarlosmillord@gmail.com>
2020-09-25 15:04:55 -0400

    Added entities for the project

---
+++ jmillord-accelerated-restaurant/src/entities/Client.ts
@@ -0,0 +1,22 @@
+import { User } from './User'
+
+
+export class Client extends User {
+  private code: string
+  constructor(id: string) {
+    super(id)
+  }
+
+  public Client(id: string) {
+    return id
+  }
+
+  public getCode() {
+    return this.code
+  }
+
+  public setCode(code: string) {
+    return this.code = code
+  }
+
+}
