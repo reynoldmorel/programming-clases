77b02bea666cace70e8618ed9db4c9cbbd3aa196
unknown <diana2monegro1@gmail.com>
2020-11-11 15:27:36 -0300

    Adding initial services interface and implementation of Employee

---
+++ monegros-restaurant-dianamonegrodiaz/src/monegros/restaurant/Services/Interfaces/IEmployeeService.java
@@ -0,0 +1,9 @@
+package monegros.restaurant.services.interfaces;
+
+import java.util.Optional;
+import monegros.restaurant.entities.Employee;
+
+public interface IEmployeeService {
+	Optional<Employee> insert(Employee employee);
+	Optional<Employee> findById(Long id);
+}
