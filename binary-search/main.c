84e9395a09ebcbc8a93dff6ddf39b573948cf62e
eapg <elidopg@gmail.com>
2021-02-13 22:37:39 -0400

    git

--- binary-search/main.c
+++ binary-search/main.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define TOTAL_ELEMENTS 10
-int numbers[] = {10, 20, 35, 80, 100, 110, 120, 121, 136, 150};
-
-int search(int val_to_find);
-int binary_search(int val_to_find, int lower_bound, int higher_bound, int half);
-
-int main(int argc, char ** argv)
+int binarySearch(int Arr[], int n, int x)
 {
-    int val_to_find = 10;
-    printf("-----------search()--------------\n");
-    int index_found = search(val_to_find);
-    printf("index_found = %d\n", index_found);
-    printf("-----------binary_search()--------------\n");
-    int index_found_by_binary_search = binary_search(val_to_find, 0, TOTAL_ELEMENTS - 1, (TOTAL_ELEMENTS - 1) / 2);
-    printf("index_found_by_binary_search = %d\n", index_found_by_binary_search);
-    return 0;
-}
 
-int search(int val_to_find)
-{
-    if(val_to_find == numbers[TOTAL_ELEMENTS - 1]) {
-        return TOTAL_ELEMENTS - 1;
-    }
+    int low = 0, high = n - 1;
 
-    for(int i = 0; i < TOTAL_ELEMENTS; i++) {
-        printf("i = %d\n", i);
-        if(numbers[i] == val_to_find) {
-            return i;
+    while (low <= high)
+    {
+
+        int mid = (low + high)/2;  
+
+        if (x == Arr[mid]) {
+            return mid;
+        }
+
+        else if (x < Arr[mid]) {
+            high = mid - 1;
+        }
+
+        else {
+            low = mid + 1;
         }
     }
 
     return -1;
 }
 
-int binary_search(int val_to_find, int lower_bound, int higher_bound, int half)
+int main(void)
 {
-    printf("half = %d\n", half);
-    if(half < lower_bound || half > higher_bound) {
-        return -1;
+    int Arr[] = { 2, 5, 6, 8, 9, 10 };
+    int target = 2;
+
+    int n = sizeof(Arr)/sizeof(Arr[0]);
+    int index = binarySearch(Arr, n, target);
+
+    if (index != -1) {
+        printf("Element found at index %d", index);
+    }
+    else {
+        printf("Element not found in the array");
     }
 
-    if(val_to_find == numbers[half]) {
-        return half;
-    } else if(val_to_find > numbers[half]) {
-        lower_bound = half + 1;
-        return binary_search(val_to_find, lower_bound, higher_bound, (higher_bound + lower_bound) / 2);
-    } else if(val_to_find < numbers[half]) {
-        higher_bound = half - 1;
-        return binary_search(val_to_find, lower_bound, higher_bound, (higher_bound + lower_bound) / 2);
-    }
-}
+    return 0;
+} 
