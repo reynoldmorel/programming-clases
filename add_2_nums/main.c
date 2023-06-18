087437b9a6502af5f93cf15bf36e20534f9563f3
Reynold Morel <reynoldmorel@gmail.com>
2023-06-18 08:55:43 -0400

    Add 2 nums program

--- add_2_nums/main.c
+++ add_2_nums/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+int add(int n1, int n2);
 
-int main() {
+int main()
+{
     printf("Hello World!");
-
+    int result = add(1, 2);
     return 0;
+}
+
+int add(int n1, int n2)
+{
+    return n1 + n2;
 }
